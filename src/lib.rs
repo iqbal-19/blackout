4c53e613931ef5c04601d6d0ca69de86fca9c49e
iqbal-19 <iqbalpratamanis@gmail.com>
2026-06-29 00:35:52 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,33 @@ use worker::*;
 
 #[event(fetch)]
 async fn fetch(
-    _req: Request,
-    _env: Env,
+    req: Request,
+    env: Env,
     _ctx: Context,
 ) -> Result<Response> {
-    Response::ok("Rust OK")
+    let url = req.url()?;
+
+    match url.path() {
+        "/" => {
+            let main_url = env.var("MAIN_PAGE_URL")?.to_string();
+
+            let mut resp = Fetch::Url(main_url.parse()?).send().await?;
+
+            Response::from_html(resp.text().await?)
+        }
+
+        "/sub" => {
+            let sub_url = env.var("SUB_PAGE_URL")?.to_string();
+
+            let mut resp = Fetch::Url(sub_url.parse()?).send().await?;
+
+            Response::from_html(resp.text().await?)
+        }
+
+        "/link" => {
+            Response::ok("Coming Soon")
+        }
+
+        _ => Response::error("404 Not Found", 404),
+    }
 }
